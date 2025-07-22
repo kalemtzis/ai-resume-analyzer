15adb64e41ed8e37c2fe1ed16df6cd0d79281986
kalemtzis <kalemtzis@csd.auth.gr>
2025-07-22 08:39:01 +0300

    Add log out button in homepage

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,10 @@
+import { useEffect } from "react";
 import { Link } from "react-router";
+import { usePuterStore } from "~/lib/puter";
 
 const Navbar = () => {
+    const { auth } = usePuterStore();
+
     return (
         <nav className="navbar">
             <Link to='/'>
@@ -9,6 +13,15 @@ const Navbar = () => {
             <Link to='/upload' className="primary-button w-fit">
                 Upload Resume
             </Link>
+            {auth.isAuthenticated ? (
+                <button className="primary-button w-fit h-fit" onClick={auth.signOut}>
+                    Log Out
+                </button>
+            ) : (
+                <button className="primary-button w-fit h-fit" onClick={auth.signIn}>
+                    Log In
+                </button>
+            )}
         </nav>
     )
 }
